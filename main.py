b2df95ba0a9f2dcd219ddadb052736a5a9239e17
melliott90 <elliott@calisuntech.com>
2015-09-13 11:48:20 -0700

    update

--- main.py
+++ main.py
@@ -15,10 +15,25 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import urllib
+
+
+
+#template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getcwd()))
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Wow this actually works!!')
+        template = JINJA_ENVIRONMENT.get_template('home.html')
+        self.response.write(template.render() )
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
